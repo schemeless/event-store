b36ab7e70d6d1724c3690e0d333d74ebdcc359d2
Liqiu Huang <akinoniku@gmail.com>
2021-06-02 22:22:36 +1200

    add null adapter

---
+++ packages/event-store-adapter-null/src/EventStore.repo.ts
@@ -0,0 +1,22 @@
+import type { CreatedEvent, IEventStoreEntity, IEventStoreRepo } from '@schemeless/event-store-types';
+import { logger } from './utils/logger';
+
+export class EventStoreRepo implements IEventStoreRepo {
+  constructor() {}
+
+  async init() {}
+
+  async getAllEvents(pageSize: number = 0): Promise<AsyncIterableIterator<Array<IEventStoreEntity>>> {
+    throw new Error('Not implemented');
+  }
+
+  createEventEntity(event: CreatedEvent<any>) {
+    return event as IEventStoreEntity;
+  }
+
+  storeEvents = async (events: CreatedEvent<any>[]) => {};
+
+  resetStore = async () => {
+    logger.info('not needed');
+  };
+}
