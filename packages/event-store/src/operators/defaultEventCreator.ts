df438423436257a9eed4c05f709f598697221d82
Liqiu Huang <akinoniku@gmail.com>
2025-09-26 13:23:50 +1200

    Fix causation id fallback and add regression test (#47)

--- packages/event-store/src/operators/defaultEventCreator.ts
+++ packages/event-store/src/operators/defaultEventCreator.ts
@@ -1,10 +1,10 @@
 import type { BaseEvent, CreatedEvent } from '@schemeless/event-store-types';
 import { getUlid } from '../util/ulid';
 
-const dateDefault = (date: string | Date | undefined): Date =>{
-  if (!date) return new Date()
-  return typeof date === 'string' ? new Date(date) : date
-}
+const dateDefault = (date: string | Date | undefined): Date => {
+  if (!date) return new Date();
+  return typeof date === 'string' ? new Date(date) : date;
+};
 
 export function defaultEventCreator<Payload>(
   eventArgs: BaseEvent<Payload>,
@@ -14,10 +14,10 @@ export function defaultEventCreator<Payload>(
   return {
     ...eventArgs,
     id,
-    causationId: eventArgs.causationId || causalEvent ? causalEvent.id : undefined,
+    causationId: eventArgs.causationId ?? (causalEvent ? causalEvent.id : undefined),
     correlationId: eventArgs.correlationId || (causalEvent ? causalEvent.correlationId || causalEvent.id : id),
     identifier: eventArgs.identifier || (causalEvent ? causalEvent.identifier : undefined),
 
-    created: dateDefault(eventArgs.created)
+    created: dateDefault(eventArgs.created),
   };
 }
