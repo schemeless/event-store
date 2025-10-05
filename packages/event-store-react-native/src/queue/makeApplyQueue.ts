a81051688ca7d4d50bcf785562d1888301338652
akinoniku <akinoniku@gmail.com>
2025-10-06 04:11:52 +1300

    fix: switch UUID generation from v4 to v1 in makeApplyQueue

--- packages/event-store-react-native/src/queue/makeApplyQueue.ts
+++ packages/event-store-react-native/src/queue/makeApplyQueue.ts
@@ -1,6 +1,6 @@
 import { createRxQueue } from './RxQueue';
 import type { BaseEvent, CreatedEvent } from '@schemeless/event-store-types';
-import { v4 as uuid } from 'uuid';
+import { v1 as uuid } from 'uuid';
 
 export const makeApplyQueue = () =>
   createRxQueue<{ causalEvent?: CreatedEvent<any>; currentEvent: BaseEvent<any> }, CreatedEvent<any>>(
