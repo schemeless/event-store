e94799bda4675865049004187ebbd87149d31313
akinoniku <akinoniku@gmail.com>
2026-02-03 19:59:38 +1300

    chore(release): prepare for 2.6.0

--- packages/event-store/jest.config.js
+++ packages/event-store/jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
   },
   moduleNameMapper: {
-    '^@schemeless/(.*)$': '<rootDir>/../$1/src'
-  }
+    '^@schemeless/(.*)$': '<rootDir>/../$1/src',
+  },
 };
